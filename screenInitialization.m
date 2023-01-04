aeefc9b64be0a40f6c5f771ac680c5d47686010d
Camille Elleaume <ca.elleaume@gmail.com>
2023-01-04 17:03:49 +0100

    delete useless function

--- screenInitialization.m
+++
@@ -1,8 +0,0 @@
-function settings = screenInitialization()
-
-    settings.layout.color.background = [255 255 255];
-    settings.layout.color.text = [0 0 0];
-       
-    % settings. general.odd = [4, 5, 6];
-    % settings.sound.srate = 150;
-end
