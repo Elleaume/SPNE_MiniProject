958be821e68d1a4ab30b3b6ff151ef810ec6eb24
Camille Elleaume <ca.elleaume@gmail.com>
2023-01-04 15:30:05 +0100

    Almost there - Have to clean up

---
+++ waitForEnter.m
@@ -0,0 +1,13 @@
+function waitForEnter()
+    WaitSecs(0.2);
+    keyIsDown = 0;
+    while ~keyIsDown 
+        [keyIsDown, seconds, key_code] = KbCheck();
+        if keyIsDown
+            key = KbName([key_code])
+            if key == 'Return'
+                break
+            end
+        end
+    end
+end
